d029b5d75c0246a4b1dc680071d6374c1fa5f124
warren <warren.aroni@innovar.io>
2018-05-29 09:20:52 -0500

    gg

--- src/main/java/innovar/io/grifo/entity/Product.java
+++ src/main/java/innovar/io/grifo/entity/Product.java
@@ -3,14 +3,8 @@ package innovar.io.grifo.entity;
 
 import lombok.*;
 import lombok.experimental.Accessors;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,12 +16,10 @@ public class Product {
     @TextIndexed
     private String code;
     @TextIndexed
-    private String name;
-    @TextIndexed
     private String description;
-    private UnitCost unitCost;
-    private Long stock;
+
     private String measureUnit;
+    private Double unitaryValue;
 
 }
 
